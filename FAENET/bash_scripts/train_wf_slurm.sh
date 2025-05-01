2f77d84bd1c433a439887b179e6e903b27c057ae
karan-m <karan.mudaliar1999@gmail.com>
2025-04-30 23:00:01 -0400

    config bash script

--- FAENET/bash_scripts/train_wf_slurm.sh
+++ FAENET/bash_scripts/train_wf_slurm.sh
@@ -63,7 +63,7 @@ python -m faenet.train \
   --weight_decay=1e-5 \
   --dropout=0.15 \
   --no-consistency-loss \
-  --use_mlflow=True \
+  --use-mlflow \
   --mlflow_experiment_name=FAENet_WF_Predictions \
   --run_name=WF_Production_Run \
   --output_dir=/home/mudaliar.k/github/Symmetry-Breaking-Frame-Averaging/outputs/WF_Production
